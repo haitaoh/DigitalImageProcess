e58582860b524830d62af4df3bd7e824fec250e6
haith <shuhaith@foxmail.com>
2016-12-20 16:57:24 +0800

    test

--- DigitalImageProcess/MainWindow.h
+++ DigitalImageProcess/MainWindow.h
@@ -3,6 +3,9 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_MainWindow.h"
 
+#include "opencv\cv.hpp"
+#include "opencv2\highgui\highgui.hpp"
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
